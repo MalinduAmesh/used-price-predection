a4b9597543cd721646905ecc491f4d25784d5337
Malindu Amesh <malindu@misynergy.com>
2022-07-15 00:49:55 +0530

    final phase

--- application.py
+++ application.py
@@ -1,10 +1,34 @@
-from flask import Flask
+from flask import Flask,render_template,request
+import pandas as pd
+import pickle
+import numpy as np
 
 app=Flask(__name__)
 
+model = pickle.load(open("LinearRegressionModel.pkl",'rb'))
+car=pd.read_csv("Car Dataset.csv")
+
 @app.route('/')
 def index():
-    return "Hello World"
+    companies = sorted(car['company'].unique())
+    car_models = sorted(car['name'].unique())
+    year = sorted(car['year'].unique())
+    fuel_type = sorted(car['fuel_type'].unique())
+    return render_template('index.html',companies = companies, car_models=car_models,year=year,fuel_type=fuel_type)
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    company = request.form.get('company')
+    car_model = request.form.get('car_model')
+    year = int(request.form.get('year'))
+    fuel_type = request.form.get('fuel_type')
+    kms_driven = int(request.form.get('kilo_driven'))
+    print(company,car_model,year,fuel_type,kms_driven)
+
+    prediction = model.predict(pd.DataFrame([[car_model,company,year,fuel_type,kms_driven]],columns=['name','company'
+        ,'year','fuel_type','kms_driven']))
+    print(prediction)
+    return str(np.round(prediction[0],2))
 
 if __name__=="__main__":
     app.run(debug=True)
